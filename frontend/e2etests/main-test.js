af7915636dcda2198dba0d96ea0806491a2b4fb1
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-07-17 23:37:27 +0300

    again vol 8

---
+++ frontend/e2etests/main-test.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+describe("Note app", function () {
+  beforeEach(function () {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("front page can be opened", function () {
+    cy.contains("ecommerce");
+  });
+
+  it("login form can be opened", function () {
+    cy.contains("login").click();
+  });
+
+  it("user can log in", function () {
+    cy.contains("login").click();
+    cy.get("#nameInput").type("testi timo");
+    cy.get("#passwordInput").type("testitimo");
+    cy.contains("Log In").click();
+    cy.contains("logout");
+  });
+
+  it("products are loaded from db", function () {
+    cy.contains("Products").click();
+    cy.contains("Add to cart");
+    cy.get("#searchProduct").type("shir");
+    cy.contains("Add to cart");
+  });
+});
