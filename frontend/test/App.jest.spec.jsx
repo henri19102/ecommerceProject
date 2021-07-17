87fec1315239f230f5ee39620bfe88b9644457c2
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-07-18 00:38:00 +0300

    kansioita mikä ongelma

--- frontend/test/App.jest.spec.jsx
+++ frontend/test/App.jest.spec.jsx
@@ -1,15 +1,25 @@
 describe("product data is got", () => {
-  const product = [
+  const products = [
     {
       name: "shirt",
       price: 10,
-      count: 2,
+      count: 1,
       category: "Attire"
-    }
+    },
+    {
+      name: "lamborghini",
+      price: 258000,
+      count: 1,
+      category: "Car"
+    },
   ];
 
   test("when list has only one item, get price for that", () => {
-    expect(product[0].price).toBe(10);
+    expect(products[0].price).toBe(10);
+  });
+
+  test("when list has multiple items,get total price", () => {
+    expect(products.reduce((x,y) => x.price+y.price),0).toBe(258010);
   });
 });
 
