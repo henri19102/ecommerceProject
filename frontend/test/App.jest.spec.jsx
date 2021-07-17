77865879160653a5ec4010e06efbf71eb3644a03
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-07-18 00:24:00 +0300

    test upgrade

--- frontend/test/App.jest.spec.jsx
+++ frontend/test/App.jest.spec.jsx
@@ -1,8 +1,15 @@
-test('palindrome of a', () => {
-  const product =  {
-    name: "shirt",
-    price: 10,
-    count: DataTypes.INTEGER,
-    category: DataTypes.STRING
-  }
-})
+describe("product data is got", () => {
+  const product = [
+    {
+      name: "shirt",
+      price: 10,
+      count: 2,
+      category: "Attire"
+    }
+  ];
+
+  test("when list has only one item, get price for that", () => {
+    expect(product[0].price).toBe(10);
+  });
+});
+
