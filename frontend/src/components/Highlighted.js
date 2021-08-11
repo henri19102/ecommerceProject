1bc7a47cc0e268cda5ecc3f07d669e33862405f9
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-08-11 07:15:36 +0300

    Updated Highligted

--- frontend/src/components/Highlighted.js
+++ frontend/src/components/Highlighted.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useProducts } from "./reducers/ProductsReducer";
-import { Grid, Paper } from "@material-ui/core";
+import { Grid, Paper, Tooltip } from "@material-ui/core";
 import { useStyles } from "../styles/styles";
+import { useHistory } from "react-router-dom";
 
 const shuffleArray = (array) => {
   for (var i = array.length - 1; i > 0; i--) {
@@ -12,25 +13,35 @@ const shuffleArray = (array) => {
   }
 };
 
-
 const Highlighted = () => {
   const { products } = useProducts();
+  const history = useHistory();
+  const classes = useStyles();
   if (!products) return null;
 
-  const classes = useStyles();
   shuffleArray(products);
   const unique = [...new Set(products)];
-
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>Myydyimmät tuotteet</Paper>
+          <Paper variant='outlined' className={classes.paper}>
+            Myydyimmät tuotteet
+          </Paper>
         </Grid>
         {unique.slice(0, 4).map((x) => (
           <Grid key={x.id} item xs={3}>
-            <Paper className={classes.paper}>{x.name}</Paper>
+            <Tooltip title={`Only ${x.price}€ Click to view in detail!`}>
+              <Paper
+                id="higlight-paper"
+                onClick={() => history.push(`products/${x.id}`)}
+                elevation={10}
+                className={classes.paper}
+              >
+                {x.name}
+              </Paper>
+            </Tooltip>
           </Grid>
         ))}
       </Grid>
