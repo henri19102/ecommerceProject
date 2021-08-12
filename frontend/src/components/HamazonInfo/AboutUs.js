0e2508f62f4d9cea70763c3589409e33888fb786
Macezu <macez@hotmail.com>
2021-08-12 10:45:53 +0300

    googlemaps added

--- frontend/src/components/HamazonInfo/AboutUs.js
+++ frontend/src/components/HamazonInfo/AboutUs.js
@@ -7,7 +7,7 @@ import {
   List,
   ListItem,
   ListItemIcon,
-  ListItemText
+  ListItemText,
 } from "@material-ui/core";
 import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
 import PhoneRoundedIcon from "@material-ui/icons/PhoneRounded";
@@ -44,19 +44,21 @@ const AboutUs = () => {
             </p>
             <p>
               We are your relatives im not sure, try something else we try your
-              eye, but can you change everything?. It&apos;s great, can you
-              add a beard though jazz it up a little, yet can you lower the
-              price for the website? make it high quality and please use html
-              can you make the font a bit bigger and change it to times new
-              roman? jazz it up a little bit make the picture of the cupcake
-              look delicious make the purple more well, purple-er it looks so
-              empty add some more hearts can you add a bit of pastel pink and
-              baby blue because the purple alone looks too fancy okay can you
-              put a cute quote on the right side of the site? oh no it looks
-              messed up! i think we need to start from scratch. Can you make it
-              pop can you put &quot;find us on facebook&quot; by the
-              facebook logo? Jazz it up a little. That&apos;s great, but we
-              need to add this 2000 line essay. Can you make it stand out more?.
+              eye, but can you change everything?. It&apos;s great, can you add
+              a beard though jazz it up a little, yet can you lower the price
+              for the website? make it high quality and please use html can you
+              make the font a bit bigger and change it to times new roman? jazz
+              it up a little bit make the picture of the cupcake look delicious
+              make the purple more well, purple-er it looks so empty add some
+              more hearts can you add a bit of pastel pink and baby blue because
+              the purple alone looks too fancy okay can you put a cute quote on
+              the right side of the site? oh no it looks messed up! i think we
+              need to start from scratch. Can you make it pop can you put
+              &quot;find us on facebook&quot; by the facebook logo? Jazz it up a
+              little. That&apos;s great, but we need to add this 2000 line
+              essay. Can you make it stand out more?.
+            </p>
+            <p>
               Can you lower the price for the website? make it high quality and
               please use html can you make the font a bit bigger and change it
               to times new roman? jazz it up a little bit make the picture of
@@ -76,35 +78,40 @@ const AboutUs = () => {
         </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper}>
-            <List component='nav' aria-label='main mailbox folders'>
+            <List component="nav" aria-label="main mailbox folders">
               <ListItem>
                 <ListItemIcon>
                   <BusinessRoundedIcon />
                 </ListItemIcon>
-                <ListItemText primary='Tegridy Inc' />
+                <ListItemText primary="Tegridy Inc" />
               </ListItem>
               <ListItem>
                 <ListItemIcon>
                   <LocationOnRoundedIcon />
                 </ListItemIcon>
-                <ListItemText primary='TegridyRoad 160 SF CA' />
+                <ListItemText primary="TegridyRoad 160 SF CA" />
               </ListItem>
               <ListItem button component="a" href="mailto:havesome@tegridy.org">
                 <ListItemIcon>
                   <EmailRoundedIcon />
                 </ListItemIcon>
-                <ListItemText primary='havesome@tegridy.org' />
+                <ListItemText primary="havesome@tegridy.org" />
               </ListItem>
               <ListItem>
                 <ListItemIcon>
                   <PhoneRoundedIcon />
                 </ListItemIcon>
-                <ListItemText primary='339-7474809' />
+                <ListItemText primary="339-7474809" />
               </ListItem>
             </List>
           </Paper>
           <Divider />
-          <Paper className={classes.paper}>GoogleMaps Here?</Paper>
+          <Paper className={classes.paper}>
+            <iframe
+              height="600"
+              src="https://maps.google.com/maps?q=Thinadhoo&t=&z=13&ie=UTF8&iwloc=&output=embed"
+            ></iframe>
+          </Paper>
         </Grid>
       </Grid>
     </Box>
