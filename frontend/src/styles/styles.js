902cd1fe254cd4aded3a82466cdb9305676759d0
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-08-12 18:02:52 +0300

    Added visuals to products

--- frontend/src/styles/styles.js
+++ frontend/src/styles/styles.js
@@ -8,7 +8,7 @@ export const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     backgroundColor: "wheat",
     paddingTop: "1%",
-    paddingBottom: "2%",
+    paddingBottom: "2%"
   },
   footer: {
     padding: "2%",
@@ -17,44 +17,45 @@ export const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#3f51b5",
-    paddingTop: "1%",
+    paddingTop: "1%"
   },
   productSearch: {
-    backgroundColor: "white",
+    backgroundColor: "white"
   },
   toolbar: {
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   logIn: {
     display: "flex",
     justifyContent: "center",
     backgroundColor: "white",
-    padding: "2%",
+    padding: "2%"
   },
   form: {
     display: "flex",
-    flexDirection: "column",
+    flexDirection: "column"
   },
   input: {
-    margin: "5%",
+    margin: "5%"
   },
   header: {
     marginLeft: "2%",
-    marginRight: "2%",
+    marginRight: "2%"
   },
   white: {
-    color: "white",
+    color: "white"
   },
   grey: {
     backgroundColor: "grey",
-    marginTop: "0",
+    marginTop: "0"
   },
   subscribe: {
     padding: "2%",
     color: "white",
     flexWrap: "wrap",
-    background: "linear-gradient(180deg, rgba(5,5,5,1) 88%, rgba(63,81,181,1) 100%)",
+    background:
+      "linear-gradient(180deg, rgba(5,5,5,1) 88%, rgba(63,81,181,1) 100%)",
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-around",
@@ -75,35 +76,40 @@ export const useStyles = makeStyles((theme) => ({
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "space-around",
-    marginBottom: "2%",
+    marginBottom: "2%"
   },
   productsPage: {
     display: "flex",
-    flexDirection: "column",
+    flexDirection: "column"
   },
   productCard: {
     marginBottom: "2%",
+    transition: "0.2s",
+    "&:hover": {
+      transform: "scale(1.03)",
+      boxShadow: "0 4px 10px 0 #3f51b5"
+    }
   },
   green: {
     border: "solid",
     borderRadius: "5px",
-    padding: "2px",
+    padding: "2px"
   },
   message: {
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   blue: {
-    backgroundColor: "#3f51b5",
+    backgroundColor: "#3f51b5"
   },
   cardContent: {
     display: "flex",
     justifyContent: "center",
     flexDirection: "column",
-    marginBottom: "2%",
+    marginBottom: "2%"
   },
   text1: {
-    textAlign: "center",
+    textAlign: "center"
   },
   sum: {
     border: "solid",
@@ -112,29 +118,29 @@ export const useStyles = makeStyles((theme) => ({
     padding: "2px",
     backgroundColor: "white",
     textAlign: "center",
-    marginBottom: "2%",
+    marginBottom: "2%"
   },
   cardContent2: {
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   card1: {
     marginTop: "2%",
-    marginBottom: "2%",
+    marginBottom: "2%"
   },
   cardContent3: {
     width: "50%",
-    marginBottom: "2%",
+    marginBottom: "2%"
   },
   cardContent4: {
     width: "90%",
-    marginBottom: "2%",
+    marginBottom: "2%"
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: "center",
     backgroundColor: "#white",
-    color: theme.palette.text.primary,
+    color: theme.palette.text.primary
   },
   mostSelled: {
     padding: theme.spacing(2),
@@ -143,12 +149,12 @@ export const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.primary,
     "&:hover": {
       transform: "translate(0%,-5%)",
-      backgroundColor: "#99f",
-    },
+      backgroundColor: "#99f"
+    }
   },
   highlightHeader: {
     fontSize: "1.5vw",
     padding: theme.spacing(1),
-    textAlign: "center",
-  },
+    textAlign: "center"
+  }
 }));
