f55cb6b63e473c12784d005e4b514f844e5c7a46
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-08-17 15:20:43 +0300

    fixed missing spaces

--- frontend/src/components/admin/users/account/AccountCart.js
+++ frontend/src/components/admin/users/account/AccountCart.js
@@ -4,7 +4,7 @@ import { Box } from "@material-ui/core";
 const AccountCart = ({ product }) => {
   return (
     <Box>
-      ({product.productCount}) {product.productCount > 1 ? "pcs" : "pc"} of
+      ({product.productCount}) {product.productCount > 1 ? "pcs" : "pc"} {"of "}
       {product.Product.name} {`${product.Product.price} € `}
     </Box>
   );
