477753fffffdf5cc0bbddb33b460bcadb55e2f5b
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-08-23 22:00:19 +0300

    Init FireBase for profilepictures

--- frontend/src/components/admin/users/account/ProfilePicture.js
+++ frontend/src/components/admin/users/account/ProfilePicture.js
@@ -1,6 +1,23 @@
 import React from "react";
 import { Box, Avatar } from "@material-ui/core";
 import { useStyles } from "../../../../styles/styles";
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// Set the configuration for your app
+// TODO: Replace with your app's config object
+const firebaseConfig = {
+  apiKey: "<your-api-key>",
+  authDomain: "<your-auth-domain>",
+  databaseURL: "<your-database-url>",
+  storageBucket: "gs://ecommerce-react-edb89.appspot.com"
+};
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+const storage = getStorage(firebaseApp);
+console.log(storage);
+
 
 const ProfilePicture = () => {
   const classes = useStyles();
